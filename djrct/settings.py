fd239aa84d4ff8d5fcf0dadeb8fa852955d6111f
Harivatsa <gaharivatsa@gmail.com>
2023-09-28 22:03:31 +0530

    'addedstatic'

--- djrct/settings.py
+++ djrct/settings.py
@@ -124,6 +124,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Replace with the actual path to your static files directory
+]
+
+# (Optional) Configure the directory where Django will collect static files
+STATIC_ROOT = BASE_DIR / "static_root" 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
