b54dab43873c9a3963db9138bbbd883115cbe0c1
Harivatsa <gaharivatsa@gmail.com>
2023-09-28 22:11:16 +0530

    'addedstatic1'

--- djrct/settings.py
+++ djrct/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,15 +122,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = "static/"
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Replace with the actual path to your static files directory
-]
-
-# (Optional) Configure the directory where Django will collect static files
-STATIC_ROOT = BASE_DIR / "static_root" 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
