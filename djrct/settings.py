f6a12cfc237609896ba9f62f8017dd9e05e1aac5
Harivatsa <gaharivatsa@gmail.com>
2023-09-28 09:51:20 +0530

    db1

--- djrct/settings.py
+++ djrct/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = "djrct.wsgi.application"
 DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'railway',
         'USER': 'postgres',
         'PASSWORD': '7THNUFAGssTAgDjxkRUu',
